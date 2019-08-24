3bdf4fe69c6dcd049f367455982ed86e71b80cb7
Chris <csalois@gmail.com>
2019-08-23 23:23:11 -0400

    fixes bug in merge_sort

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,9 +1,10 @@
 def merge_sort(ary)
   return ary if ary.length == 1
-
-  left, right = ary.each_slice((ary.length/2.0).round).to_a.each{|ary| merge_sort(ary)}
-  sorted_ary = []
-  sorted_ary << ( left[0] < right[0] ?  left.shift : right.shift ) while left[0] && right[0]
-  sorted_ary += left + right
+  left, right = ary.each_slice((ary.length/2.0).round).to_a.map {|half| merge_sort(half)}
+  ary = []
+  ary << (left[0] < right[0] ? left.shift : right.shift) while left[0] && right[0]
+  ary += left + right
 end
 
+ex_ary = [1,-1,0,3,2,4,5]
+p merge_sort(ex_ary)
